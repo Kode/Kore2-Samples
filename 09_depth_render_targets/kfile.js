3f0450f0629cf2fb477c939d8a4b98b8bedb43ed
Robert Konrad <git2016@robdangero.us>
2024-09-20 21:50:01 +0200

    Start porting 09_depth_render_targets

--- 09_depth_render_targets/kfile.js
+++ 09_depth_render_targets/kfile.js
@@ -1,6 +1,6 @@
 const project = new Project('DepthRenderTargets');
 
-await project.addProject('../Kinc', {kong: true});
+await project.addProject('../Kinc', {kong: true, kope: true});
 
 project.addFile('Sources/**');
 project.addKongDir('Shaders');
