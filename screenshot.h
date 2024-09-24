4738ffe30d6d2ba14a675ec5cbd4e4c6ace2c84e
Robert Konrad <git2016@robdangero.us>
2024-09-25 00:32:35 +0200

    Align the copy-back-texture

--- screenshot.h
+++ screenshot.h
@@ -1,11 +1,13 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#include <kope/util/align.h>
+
 static kope_g5_buffer screenshot_buffer;
 
 static void screenshot_init_buffer(kope_g5_device *device, int width, int height) {
 	kope_g5_buffer_parameters buffer_parameters;
-	buffer_parameters.size = width * 4 * height;
+	buffer_parameters.size = kope_g5_device_align_texture_row_bytes(device, width * 4) * height;
 	buffer_parameters.usage_flags = KOPE_G5_BUFFER_USAGE_CPU_READ;
 	kope_g5_device_create_buffer(device, &buffer_parameters, &screenshot_buffer);
 }
@@ -17,7 +19,7 @@ static void screenshot_take(kope_g5_device *device, kope_g5_command_list *list, 
 
 	kope_g5_image_copy_buffer destination = {0};
 	destination.buffer = &screenshot_buffer;
-	destination.bytes_per_row = width * 4;
+	destination.bytes_per_row = kope_g5_device_align_texture_row_bytes(device, width * 4);
 
 	kope_g5_command_list_copy_texture_to_buffer(list, &source, &destination, width, height, 1);
 
