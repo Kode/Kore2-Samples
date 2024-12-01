1c65c86f05982e68ac69b0e5cec767dd0b04fca5
Robert Konrad <git2016@robdangero.us>
2024-12-01 02:47:46 +0100

    Fix 09 properly

--- 09_depth_render_targets/Sources/main.c
+++ 09_depth_render_targets/Sources/main.c
@@ -75,7 +75,7 @@ static void update(void *data) {
 
 	kope_g5_command_list_present(&list);
 
-	kope_g5_device_execute_command_list(&device, KOPE_G5_COMMAND_LIST_TYPE_GRAPHICS, &list);
+	kope_g5_device_execute_command_list(&device, &list);
 
 #ifdef SCREENSHOT
 	screenshot_take(&device, &list, framebuffer, width, height);
@@ -91,7 +91,7 @@ int kickstart(int argc, char **argv) {
 
 	kong_init(&device);
 
-	kope_g5_device_create_command_list(&device, &list);
+	kope_g5_device_create_command_list(&device, KOPE_G5_COMMAND_LIST_TYPE_GRAPHICS, &list);
 
 	kope_g5_texture_parameters texture_parameters;
 	texture_parameters.width = width;
