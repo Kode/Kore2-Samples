7f37c345ab061a941890289dbeebe2484632e269
Robert Konrad <git2016@robdangero.us>
2025-02-23 02:44:56 +0100

    Update the shader sample

--- shader/sources/shader.c
+++ shader/sources/shader.c
@@ -1,6 +1,6 @@
 #include <kinc/io/filereader.h>
 #include <kinc/system.h>
-#include <kope/graphics5/device.h>
+#include <kore3/gpu/device.h>
 
 #include <kong.h>
 
@@ -11,46 +11,57 @@
 #include "../../screenshot.h"
 #endif
 
-static kope_g5_device device;
-static kope_g5_command_list list;
+static kore_gpu_device device;
+static kore_gpu_command_list list;
 static vertex_in_buffer vertices;
-static kope_g5_buffer indices;
+static kore_gpu_buffer indices;
 
 const int width = 800;
 const int height = 600;
 
 static void update(void *data) {
-	kope_g5_texture *framebuffer = kope_g5_device_get_framebuffer(&device);
+	kore_gpu_texture *framebuffer = kore_gpu_device_get_framebuffer(&device);
 
-	kope_g5_render_pass_parameters parameters = {0};
-	parameters.color_attachments_count = 1;
-	parameters.color_attachments[0].load_op = KOPE_G5_LOAD_OP_CLEAR;
-	kope_g5_color clear_color;
-	clear_color.r = 0.0f;
-	clear_color.g = 0.0f;
-	clear_color.b = 0.0f;
-	clear_color.a = 1.0f;
-	parameters.color_attachments[0].clear_value = clear_color;
-	parameters.color_attachments[0].texture.texture = framebuffer;
-	parameters.color_attachments[0].texture.array_layer_count = 1;
-	parameters.color_attachments[0].texture.mip_level_count = 1;
-	parameters.color_attachments[0].texture.format = kope_g5_device_framebuffer_format(&device);
-	parameters.color_attachments[0].texture.dimension = KOPE_G5_TEXTURE_VIEW_DIMENSION_2D;
-	kope_g5_command_list_begin_render_pass(&list, &parameters);
+	kore_gpu_color clear_color = {
+	    .r = 0.0f,
+	    .g = 0.0f,
+	    .b = 0.0f,
+	    .a = 1.0f,
+	};
+
+	kore_gpu_render_pass_parameters parameters = {
+	    .color_attachments_count = 1,
+	    .color_attachments =
+	        {
+	            {
+	                .load_op = KORE_GPU_LOAD_OP_CLEAR,
+	                .clear_value = clear_color,
+	                .texture =
+	                    {
+	                        .texture = framebuffer,
+	                        .array_layer_count = 1,
+	                        .mip_level_count = 1,
+	                        .format = kore_gpu_device_framebuffer_format(&device),
+	                        .dimension = KORE_GPU_TEXTURE_VIEW_DIMENSION_2D,
+	                    },
+	            },
+	        },
+	};
+	kore_gpu_command_list_begin_render_pass(&list, &parameters);
 
 	kong_set_render_pipeline(&list, &pipeline);
 
 	kong_set_vertex_buffer_vertex_in(&list, &vertices);
 
-	kope_g5_command_list_set_index_buffer(&list, &indices, KOPE_G5_INDEX_FORMAT_UINT16, 0, 3 * sizeof(uint16_t));
+	kore_gpu_command_list_set_index_buffer(&list, &indices, KORE_GPU_INDEX_FORMAT_UINT16, 0, 3 * sizeof(uint16_t));
 
-	kope_g5_command_list_draw_indexed(&list, 3, 1, 0, 0, 0);
+	kore_gpu_command_list_draw_indexed(&list, 3, 1, 0, 0, 0);
 
-	kope_g5_command_list_end_render_pass(&list);
+	kore_gpu_command_list_end_render_pass(&list);
 
-	kope_g5_command_list_present(&list);
+	kore_gpu_command_list_present(&list);
 
-	kope_g5_device_execute_command_list(&device, &list);
+	kore_gpu_device_execute_command_list(&device, &list);
 
 #ifdef SCREENSHOT
 	screenshot_take(&device, &list, framebuffer, width, height);
@@ -61,12 +72,12 @@ int kickstart(int argc, char **argv) {
 	kinc_init("Example", width, height, NULL, NULL);
 	kinc_set_update_callback(update, NULL);
 
-	kope_g5_device_wishlist wishlist = {0};
-	kope_g5_device_create(&device, &wishlist);
+	kore_gpu_device_wishlist wishlist = {0};
+	kore_gpu_device_create(&device, &wishlist);
 
 	kong_init(&device);
 
-	kope_g5_device_create_command_list(&device, KOPE_G5_COMMAND_LIST_TYPE_GRAPHICS, &list);
+	kore_gpu_device_create_command_list(&device, KORE_GPU_COMMAND_LIST_TYPE_GRAPHICS, &list);
 
 	kong_create_buffer_vertex_in(&device, 3, &vertices);
 	vertex_in *v = kong_vertex_in_buffer_lock(&vertices);
@@ -85,16 +96,16 @@ int kickstart(int argc, char **argv) {
 
 	kong_vertex_in_buffer_unlock(&vertices);
 
-	kope_g5_buffer_parameters params;
+	kore_gpu_buffer_parameters params;
 	params.size = 3 * sizeof(uint16_t);
-	params.usage_flags = KOPE_G5_BUFFER_USAGE_INDEX | KOPE_G5_BUFFER_USAGE_CPU_WRITE;
-	kope_g5_device_create_buffer(&device, &params, &indices);
+	params.usage_flags = KORE_GPU_BUFFER_USAGE_INDEX | KORE_GPU_BUFFER_USAGE_CPU_WRITE;
+	kore_gpu_device_create_buffer(&device, &params, &indices);
 	{
-		uint16_t *i = (uint16_t *)kope_g5_buffer_lock_all(&indices);
+		uint16_t *i = (uint16_t *)kore_gpu_buffer_lock_all(&indices);
 		i[0] = 0;
 		i[1] = 1;
 		i[2] = 2;
-		kope_g5_buffer_unlock(&indices);
+		kore_gpu_buffer_unlock(&indices);
 	}
 
 	kinc_start();
