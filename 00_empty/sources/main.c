266287c6080758fa9143761819bd010bfef17ea6
Robert Konrad <git2016@robdangero.us>
2025-02-23 02:36:35 +0100

    Update the screenshot header

--- 00_empty/sources/main.c
+++ 00_empty/sources/main.c
@@ -20,20 +20,31 @@ static const int height = 600;
 static void update(void *data) {
 	kore_gpu_texture *framebuffer = kore_gpu_device_get_framebuffer(&device);
 
-	kore_gpu_render_pass_parameters parameters = {0};
-	parameters.color_attachments_count = 1;
-	parameters.color_attachments[0].load_op = KORE_GPU_LOAD_OP_CLEAR;
-	kore_gpu_color clear_color;
-	clear_color.r = 0.0f;
-	clear_color.g = 0.0f;
-	clear_color.b = 0.0f;
-	clear_color.a = 1.0f;
-	parameters.color_attachments[0].clear_value = clear_color;
-	parameters.color_attachments[0].texture.texture = framebuffer;
-	parameters.color_attachments[0].texture.array_layer_count = 1;
-	parameters.color_attachments[0].texture.mip_level_count = 1;
-	parameters.color_attachments[0].texture.format = KORE_GPU_TEXTURE_FORMAT_BGRA8_UNORM;
-	parameters.color_attachments[0].texture.dimension = KORE_GPU_TEXTURE_VIEW_DIMENSION_2D;
+	kore_gpu_color clear_color = {
+	    .r = 0.0f,
+	    .g = 0.0f,
+	    .b = 0.0f,
+	    .a = 1.0f,
+	};
+
+	kore_gpu_render_pass_parameters parameters = {
+	    .color_attachments_count = 1,
+	    .color_attachments =
+	        {
+	            {
+	                .load_op = KORE_GPU_LOAD_OP_CLEAR,
+	                .clear_value = clear_color,
+	                .texture =
+	                    {
+	                        .texture = framebuffer,
+	                        .array_layer_count = 1,
+	                        .mip_level_count = 1,
+	                        .format = KORE_GPU_TEXTURE_FORMAT_BGRA8_UNORM,
+	                        .dimension = KORE_GPU_TEXTURE_VIEW_DIMENSION_2D,
+	                    },
+	            },
+	        },
+	};
 	kore_gpu_command_list_begin_render_pass(&list, &parameters);
 
 	kore_gpu_command_list_end_render_pass(&list);
--- screenshot.h
+++ screenshot.h
@@ -1,37 +1,40 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-#include <kope/util/align.h>
+#include <kore3/util/align.h>
 
-static kope_g5_buffer screenshot_buffer;
+static kore_gpu_buffer screenshot_buffer;
 
-static void screenshot_take(kope_g5_device *device, kope_g5_command_list *list, kope_g5_texture *framebuffer, int width, int height) {
-	uint32_t row_bytes = kope_g5_device_align_texture_row_bytes(device, width * 4);
+static void screenshot_take(kore_gpu_device *device, kore_gpu_command_list *list, kore_gpu_texture *framebuffer, int width, int height) {
+	uint32_t row_bytes = kore_gpu_device_align_texture_row_bytes(device, width * 4);
 
-	kope_g5_buffer_parameters buffer_parameters;
-	buffer_parameters.size = row_bytes * height;
-	buffer_parameters.usage_flags = KOPE_G5_BUFFER_USAGE_CPU_READ;
-	kope_g5_device_create_buffer(device, &buffer_parameters, &screenshot_buffer);
+	kore_gpu_buffer_parameters buffer_parameters = {
+	    .size = row_bytes * height,
+	    .usage_flags = KORE_GPU_BUFFER_USAGE_CPU_READ,
+	};
+	kore_gpu_device_create_buffer(device, &buffer_parameters, &screenshot_buffer);
 
-	kope_g5_image_copy_texture source = {0};
-	source.texture = framebuffer;
-	source.mip_level = 0;
+	kore_gpu_image_copy_texture source = {
+	    .texture = framebuffer,
+	    .mip_level = 0,
+	};
 
-	kope_g5_image_copy_buffer destination = {0};
-	destination.buffer = &screenshot_buffer;
-	destination.bytes_per_row = row_bytes;
+	kore_gpu_image_copy_buffer destination = {
+	    .buffer = &screenshot_buffer,
+	    .bytes_per_row = row_bytes,
+	};
 
-	kope_g5_command_list_copy_texture_to_buffer(list, &source, &destination, width, height, 1);
+	kore_gpu_command_list_copy_texture_to_buffer(list, &source, &destination, width, height, 1);
 
-	kope_g5_device_execute_command_list(device, list);
+	kore_gpu_device_execute_command_list(device, list);
 
-	kope_g5_device_wait_until_idle(device);
+	kore_gpu_device_wait_until_idle(device);
 
-	uint8_t *pixels = (uint8_t *)kope_g5_buffer_lock_all(&screenshot_buffer);
+	uint8_t *pixels = (uint8_t *)kore_gpu_buffer_lock_all(&screenshot_buffer);
 
 	stbi_write_png("test.png", width, height, 4, pixels, row_bytes);
 
-	kope_g5_buffer_unlock(&screenshot_buffer);
+	kore_gpu_buffer_unlock(&screenshot_buffer);
 
 	exit(0);
 }
