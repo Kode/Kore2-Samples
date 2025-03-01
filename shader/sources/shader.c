60358c361c6a7fdc42c6d56a0f8718f8b728149c
Robert Konrad <git2016@robdangero.us>
2025-03-01 20:51:20 +0100

    Do not flip before the screenshot

--- shader/sources/shader.c
+++ shader/sources/shader.c
@@ -61,11 +61,11 @@ static void update(void *data) {
 
 	kore_gpu_command_list_present(&list);
 
-	kore_gpu_device_execute_command_list(&device, &list);
-
 #ifdef SCREENSHOT
 	screenshot_take(&device, &list, framebuffer, width, height);
 #endif
+
+	kore_gpu_device_execute_command_list(&device, &list);
 }
 
 int kickstart(int argc, char **argv) {
